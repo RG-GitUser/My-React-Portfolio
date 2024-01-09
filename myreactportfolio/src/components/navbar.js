7e361ef174e7dd6f42b86b1d1c8d6ab094bbc1fa
RG-GitUser <rileygaffney3@icloud.com>
2024-01-09 01:14:51 -0400

    successfully downloaded webpack for project view in server

---
+++ myreactportfolio/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
