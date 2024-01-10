72fd0d7f642ff5c6d4ad25ce47c5dcc86dc69c74
RG-GitUser <rileygaffney3@icloud.com>
2024-01-10 14:59:25 -0400

    made changes to about file

--- vite-react-portfolio/src/components/about/about.jsx
+++ vite-react-portfolio/src/components/about/about.jsx
@@ -1,11 +1,14 @@
+
 // import React from "react";
 
-function About() {
+const About = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Welcome to my portfolio! I am a passionate developer with experience in React.</p>
-        </div>
+        <section className="aboutMe">
+            <div>
+                <h1>About Page</h1>
+                <p>Welcome to my portfolio!</p>
+            </div>
+        </section>
     );
 }
 
