bd2b9fc3203ed76c895666e9cfbd8a1b62b2e16d
RG-GitUser <rileygaffney3@icloud.com>
2024-01-09 23:27:25 -0400

    attempting to troubleshoot errors with server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // Import components and pages
 
 import React from "react";
-import Navbar from '../components/navbar';
+import Navbar from './components/navbar';
 
-import About from '../pages/About';
-import Contact from '../pages/Contact'; 
-import Portfolio from '../pages/Portfolio';
+import About from './pages/About';
+import Contact from './pages/Contact'; 
+import Portfolio from './pages/Portfolio';
 
 //Function for routing pages
 function App() {
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Link } from "react-router-dom"; 
-import "../styles/index.css";
+import "./styles/index.css";
 
 
 function Navbar() {
